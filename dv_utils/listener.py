9e9aacd2096da6b05c6b0a27c495700b802b0b71
Loic Quertenmont <loic.quertenmont@gmail.com>
2023-06-20 15:55:56 +0200

    feat: fix typo

--- dv_utils/listener.py
+++ dv_utils/listener.py
@@ -35,11 +35,11 @@ class DefaultListener:
                try:
                   processor(evt)
                except Exception as err:
-                   if(log_events):
-                     audit_log("Event processing failed", evt=evt_type, state="FAILED", app="algo", error:str(err), processing_time:time.time()-start)
-              else:
                   if(log_events):
-                     audit_log("Event processing done", evt=evt_type, state="DONE", app="algo", processing_time:time.time()-start)
+                     audit_log("Event processing failed", evt=evt_type, state="FAILED", app="algo", error=str(err), processing_time=time.time()-start)
+               else:
+                  if(log_events):
+                     audit_log("Event processing done", evt=evt_type, state="DONE", app="algo", processing_time=time.time()-start)
                
           
            if not daemon:
