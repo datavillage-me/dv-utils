ce351a25075e4be428721c6d8198a40e538a5054
Arne Goeteyn <goeteynarne@gmail.com>
2023-12-13 12:19:48 +0100

    feat: update simple_connector

--- dv_utils/connectors/connector.py
+++ dv_utils/connectors/connector.py
@@ -1,3 +1,19 @@
-class Connector:
-    def get(self): 
+import copy
+
+
+class Config(object):
+    download_dir = None
+
+
+class Connector(object):
+
+    config: Config = None
+
+    def __init__(self, config: Config):
+        self.config = copy.copy(config)
+
+        if not self.config.download_dir:
+            self.config.download_dir = '/resources/data'
+
+    def get(self):
         pass
--- dv_utils/connectors/simple_file.py
+++ dv_utils/connectors/simple_file.py
@@ -1,27 +1,32 @@
-from .connector import Connector
+from .connector import Connector, Config
 from urllib.parse import urlparse
 import requests
 import logging
 
 logger = logging.getLogger(__name__)
 
-class SimpleFileConfiguration():
+
+class SimpleFileConfiguration(Config):
+    def __init__(self) -> None:
+        super().__init__()
+
     url = None
     fileName = None
 
+
 class SimpleFileConnector(Connector):
     config: SimpleFileConfiguration = None
+
     def __init__(self, config: SimpleFileConfiguration) -> None:
-        super().__init__()
-        if not config.url:
+        super().__init__(config)
+
+        if not self.config.url:
             logger.error('No url passed to configuration')
 
-        if not config.fileName:
-            config.fileName = urlparse(config.url).netloc
-
-        self.config = config
+        if not self.config.fileName:
+            self.config.fileName = urlparse(self.config.url).netloc
 
     def get(self):
         response = requests.get(self.config.url)
-        with open('/resources/data/' + self.config.fileName, 'w') as file:
+        with open(self.config.download_dir + self.config.fileName, 'w') as file:
             file.write(response.text)
