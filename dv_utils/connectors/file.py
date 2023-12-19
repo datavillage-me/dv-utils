7413336e4c02b3c5ac12acb36255728263cc5116
Arne Goeteyn <goeteynarne@gmail.com>
2023-12-19 08:56:22 +0100

    rename simple file to file

--- dv_utils/connectors/file.py
+++ dv_utils/connectors/file.py
@@ -1,28 +1,25 @@
-from .connector import Connector, Config
+from connectors.connector import Configuration
 from urllib.parse import urlparse
 import requests
 import logging
 import os
+import copy
 
 logger = logging.getLogger(__name__)
 
 
-class SimpleFileConfiguration(Config):
-    def __init__(self) -> None:
-        super().__init__()
-
+class FileConfiguration(Configuration):
+    schema_file = "file.json"
     url = None
     fileName = None
+    downloadDir = None
 
 
-class SimpleFileConnector(Connector):
-    config: SimpleFileConfiguration
+class FileConnector():
+    config: FileConfiguration
 
-    def __init__(self, config: SimpleFileConfiguration) -> None:
-        super().__init__(config)
-
-        if not self.config.url:
-            logger.error('No url passed to configuration')
+    def __init__(self, config: FileConfiguration) -> None:
+        self.config = copy.copy(config)
 
         if not self.config.fileName:
             self.config.fileName = urlparse(self.config.url).netloc
