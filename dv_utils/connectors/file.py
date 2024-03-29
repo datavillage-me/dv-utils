479e01b186b61b1e49bb2853c13d5d1c450bab56
Michiel Rogissart <michiel@datavillage.me>
2024-03-29 10:08:00 +0100

    add use_scraper to file connector and remove rss connector

--- dv_utils/connectors/file.py
+++ dv_utils/connectors/file.py
@@ -4,6 +4,7 @@ import requests
 import logging
 import os
 import copy
+import cloudscraper
 
 logger = logging.getLogger(__name__)
 
@@ -13,6 +14,7 @@ class FileConfiguration(Configuration):
     url = None
     file_name = None
     download_directory = None
+    use_scraper = "false"
 
 
 class FileConnector():
@@ -25,6 +27,32 @@ class FileConnector():
             self.config.file_name = urlparse(self.config.url).netloc
 
     def get(self):
-        response = requests.get(self.config.url)
-        with open(os.path.join(self.config.download_directory, self.config.file_name), 'w') as file:
-            file.write(response.text)
+        if(self.__should_use_scraper()):
+            scraper = cloudscraper.create_scraper()
+            response = scraper.get(self.config.url)
+        else:
+            response = requests.get(self.config.url)
+
+        is_valid = self.__handle_response(response)
+
+        if(is_valid):
+            with open(os.path.join(self.config.download_directory, self.config.file_name), 'w') as file:
+                file.write(response.text)
+
+    def __should_use_scraper(self) -> bool:
+        use_scraper_cleaned = self.config.use_scraper.strip().lower()
+        if(use_scraper_cleaned == "true"):
+            return True
+        
+        if(use_scraper_cleaned == "false"):
+            return False
+        
+        logger.warn(f"{self.config.use_scraper} is not a valid boolean value. Falling back to default `False`")
+        return False
+    
+    def __handle_response(self, response) -> bool :
+        if(response.status_code > 399):
+            logger.warn(f"Response returned status code [{response.status_code}]")
+            return False
+        
+        return True
