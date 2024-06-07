da87b0dfbb3bac32c55f6b1088d9511749fd5d4a
michielrogissart-datavillage <162133673+michielrogissart-datavillage@users.noreply.github.com>
2024-06-07 17:03:30 +0200

    FileConnector list of files

--- dv_utils/connectors/file.py
+++ dv_utils/connectors/file.py
@@ -27,17 +27,31 @@ class FileConnector():
             self.config.file_name = urlparse(self.config.url).netloc
 
     def get(self):
+        urls = self.config.url.split(',')
+        file_names = self.config.file_name.split(',')
+
+        if len(urls) != len(file_names):
+            logger.error(f'Length of urls and file list should be the same. Got {len(urls)} and {len(file_names)}')
+            return
+        
+        for i in range(len(urls)):
+            self.__get_file(urls[i].strip(), file_names[i].strip())
+    
+    def __get_file(self, url, file_name):
+        logger.info(f'Downloading {file_name} from {url}')
         if(self.config.use_scraper):
             scraper = cloudscraper.create_scraper()
-            response = scraper.get(self.config.url)
+            response = scraper.get(url)
         else:
-            response = requests.get(self.config.url)
-
+            response = requests.get(url)
+        
         is_valid = self.__handle_response(response)
-
         if(is_valid):
-            with open(os.path.join(self.config.download_directory, self.config.file_name), 'w') as file:
+            with open(os.path.join(self.config.download_directory, file_name), 'w') as file:
                 file.write(response.text)
+        else:
+            logger.error(f'Could not download file {file_name} from {url}. Got {response.status_code}')
+        logger.info('done')
     
     def __handle_response(self, response) -> bool :
         if(response.status_code > 399):
