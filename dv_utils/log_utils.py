62b707356ec57a190ff3786565adc2f8ddb24746
Michiel Rogissart <michiel@datavillage.me>
2024-07-08 15:15:39 +0200

    refactor

--- dv_utils/log_utils.py
+++ dv_utils/log_utils.py
@@ -14,60 +14,52 @@ class LogLevel(Enum):
     WARN = 30
     ERROR = 40
 
-def prepare_log(app_id:str|None, log:str|None=None, data:dict|None=None, app="operator", **extra_labels):
-    if(log is None and data is None):
-        return None, None
-    if(app_id is None):
-        app_id = os.environ.get("DV_CAGE_ID",None)
-    if(app_id is None):
-        return None, None
-
-    if(data is None):
-        data={}
-    if("message" in data):
-        log = data.pop("message")
-    if("log" in data):
-        log = data.pop("log")
-
-
-    app_hash = app_id.rsplit("-")[-1]
-    app_namespace = f"app-{app_hash}"
-    app = "algo"
-    data = {"streams": [{ "stream": { "namespace":app_namespace, "app": app, "audit":"true", **data, **extra_labels }, "values": [ [ str(time.time_ns()), log ] ] }]}
-    return app_namespace, data
-
 def get_loki_url() -> str:
     return os.environ.get("DV_LOKI", "http://loki.datavillage.svc.cluster.local:3100")
 
-# Set default value of app back to 'algo'
-def audit_log(log:str|None=None, data:dict|None=None, app_id:str|None=None, app="michiel-local", level:LogLevel = LogLevel.INFO, **extra_labels):
-    log_dict = {'level': level.name}
-    log_dict.update({'log': log} if type(log) == str else log)
+def get_app_namespace() -> str | None:
+    cage_id = os.environ.get('DV_CAGE_ID', None)
+    if not cage_id:
+        return None
+    else:
+        return f'app-{cage_id}'
 
-    log_json = {"streams": [{ "stream": { "app": f"{app}" }, "values": [ [ str(time.time_ns()), str(log_dict) ] ] }]}
-    app_namespace, _ = prepare_log(app_id, '', None)
+def get_app_id() -> str | None:
+    return os.environ.get('DV_APP_ID', None)
 
+def create_body(log: str | dict | None, level: LogLevel):
+    log_dict = {'log': log} if type(log) == str else log 
+    log_dict.update({'level': level.name})
+
+    app_id = get_app_id()
+    return {"streams": [{ "stream": { "app": f"{app_id}" }, "values": [ [ str(time.time_ns()), str(log_dict) ] ] }]} 
+
+def audit_log(log:str|dict|None=None, level:LogLevel = LogLevel.INFO):
     loki_url = get_loki_url()
     if loki_url.upper() == 'STDOUT':
         print(log)
     elif loki_url.upper() == 'STDERR':
         print(log, file=sys.stderr)
-    elif(app_namespace):
-        __push_loki(loki_url, log_json, app_namespace)
     else:
-        # code shouldn't get here
-        pass
+        data = create_body(log, level)
+        __push_loki(loki_url, data)
        
 
-def __push_loki(url: str, data: dict, app_namespace: str):
+def __push_loki(url: str, data: dict):
+    app_namespace = get_app_namespace()
+
     r = httpx.post(url=f'{url}/loki/api/v1/push', json=data, headers={"X-Scope-OrgID": app_namespace, "Content-Type": "application/json"})
     if(r.status_code!=204):
         print(f"Error pushing log {r}", flush=True) 
 
-async def audit_log_async(log:str|None=None, data:dict|None=None, app_id:str|None=None, app="algo", **extra_labels):
-    app_namespace, data = prepare_log(app_id=app_id, log=log, data=data, app=app, **extra_labels)
-    if(app_namespace):
-       async with httpx.AsyncClient() as client:
-           r = await client.post(url=f'{get_loki_url()}/loki/api/v1/push', json=data, headers={"X-Scope-OrgID": app_namespace, "Content-Type": "application/json"})
-           if(r.status_code!=204):
-               print(f"Error pushing log {r}", flush=True)
+async def audit_log_async(log:str|dict|None=None, level: LogLevel = LogLevel.INFO):
+    loki_url = get_loki_url()
+    if (loki_url == 'STDOUT' or loki_url == 'STDERR'):
+        audit_log(log, level)
+    else:
+        app_namespace = get_app_namespace()
+        body = create_body(log, level)
+        async with httpx.AsyncClient() as client:
+            r = await client.post(url=f'{get_loki_url()}/loki/api/v1/push', json=body, headers={"X-Scope-OrgID": app_namespace, "Content-Type": "application/json"})
+            if(r.status_code!=204):
+                print(f"Error pushing log {r}", flush=True)
