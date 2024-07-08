d1076fe4a3f889e0031b7319ba1e469214249f2c
Michiel Rogissart <michiel@datavillage.me>
2024-07-08 15:26:15 +0200

    set app to algo

--- dv_utils/log_utils.py
+++ dv_utils/log_utils.py
@@ -24,15 +24,11 @@ def get_app_namespace() -> str | None:
     else:
         return f'app-{cage_id}'
 
-def get_app_id() -> str | None:
-    return os.environ.get('DV_APP_ID', None)
-
 def create_body(log: str | dict | None, level: LogLevel):
     log_dict = {'log': log} if type(log) == str else log 
     log_dict.update({'level': level.name})
 
-    app_id = get_app_id()
-    return {"streams": [{ "stream": { "app": f"{app_id}" }, "values": [ [ str(time.time_ns()), str(log_dict) ] ] }]} 
+    return {"streams": [{ "stream": { "app": "algo" }, "values": [ [ str(time.time_ns()), str(log_dict) ] ] }]} 
 
 def audit_log(log:str|dict|None=None, level:LogLevel = LogLevel.INFO):
     loki_url = get_loki_url()
