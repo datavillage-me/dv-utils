bc90f0fccfbf92b8ca9b66ef7874d9ce36fd4163
Michiel Rogissart <michiel@datavillage.me>
2024-08-06 17:47:35 +0200

    push loaded configs to server

--- push_connector_config.py
+++ push_connector_config.py
@@ -1,21 +1,37 @@
 import os
 import json
+import requests
 
-
-def push_config():
-  # API_URL = os.environ['DV_API_URL']
-  # DV_TOKEN = os.environ['DV_TOKEN']
-  
+def load_configs() -> list[dict]:
   connector_dir = os.path.join('dv_utils', 'connectors')
+  configs = []
 
   for file in os.listdir(connector_dir):
     if file.endswith('.json'):
       with open(os.path.join(connector_dir, file), 'r') as f:
-        schema = json.loads(f.read())['schema']
-        print(schema)
+        config = json.loads(f.read())
+        name = file[:-len('.json')]
+        config['name'] = name.capitalize()
 
+        configs.append(config)
 
-  
+  return configs
+
+def push_configs():
+  API_URL = os.environ['DV_URL']
+  DV_TOKEN = os.environ['DV_TOKEN']
+  schemas = load_configs()
+  headers = {'Authorization': f'Bearer {DV_TOKEN}'}
+  endpoint = f'{API_URL}/connectors'
+
+  for conn_config in schemas:
+    res = requests.put(endpoint, headers=headers, json=conn_config)
+    if (res.status_code < 200 or res.status_code > 299):
+      print(f'Something went wrong when pushing configuration {res.text}')
+
+  print('done')
+  exit(0)
+
 
 if __name__ == '__main__':
-  push_config()
+  push_configs()
