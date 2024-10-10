1314bc121c244f2097ea0cba63d1af4cdbaf54dc
Michiel Rogissart <michiel@datavillage.me>
2024-10-10 15:26:19 +0200

    move uma communication to solid_utils

--- dv_utils/connectors/uma.py
+++ dv_utils/connectors/uma.py
@@ -1,8 +1,7 @@
 import copy
-import requests
 
 from dv_utils.connectors.connector import Configuration
-from dv_utils import audit_log
+from dv_utils import audit_log, solid_utils
 
 class UmaConfiguration(Configuration):
   schema_file = "uma.json"
@@ -18,28 +17,11 @@ class UmaConnector():
     self.config = copy.copy(config)
 
   def get_from_pod(self, pod_url: str) -> str:
-    (uma_uri, permission_ticket) = self.__get_ticket(pod_url, self.config.path)
+    (uma_uri, permission_ticket) = solid_utils.get_permission_ticket(pod_url, self.config.path)
     print(uma_uri)
     print(permission_ticket)
     return ""
   
 
-  # TODO: create solid_utils file
-  def __get_ticket(self, pod_url: str, path: str) -> tuple[str, str]:
-    res = requests.get(f"{pod_url}/{path}")
 
-    if res.status_code != 401:
-      audit_log(f"Expected status code 401, got {res.status_code}")
-      return {}
-    
-    # UMA as_uri="https://uma...", ticket="ey...",...
-    [uma_uri_part, ticket_part] = res.headers['www-authenticate'].split(',')[:2]
-
-    uma_uri_start = uma_uri_part.index('"') + 1
-    uma_uri = uma_uri_part[uma_uri_start:-1]
-
-    ticket_start = ticket_part.index('"') + 1
-    ticket = ticket_part[ticket_start:-1]
-
-    return(uma_uri, ticket)
 
---
+++ dv_utils/solid_utils.py
@@ -0,0 +1,24 @@
+import requests
+from dv_utils import audit_log
+
+"""
+Fetches permission ticket to access a resource through UMA flow
+returns (uma_uri, permission ticket)
+"""
+def get_permission_ticket(pod_url: str, path: str) -> tuple[str, str]:
+  res = requests.get(f"{pod_url}/{path}")
+
+  if res.status_code != 401:
+    audit_log(f"Expected status code 401, got {res.status_code}")
+    return {}
+  
+  # UMA as_uri="https://uma...", ticket="ey...",...
+  [uma_uri_part, ticket_part] = res.headers['www-authenticate'].split(',')[:2]
+
+  uma_uri_start = uma_uri_part.index('"') + 1
+  uma_uri = uma_uri_part[uma_uri_start:-1]
+
+  ticket_start = ticket_part.index('"') + 1
+  ticket = ticket_part[ticket_start:-1]
+
+  return (uma_uri, ticket)
