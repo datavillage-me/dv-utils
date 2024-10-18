c92eb752223a0be60d2292ab4a8724796f98edda
Michiel Rogissart <michiel@datavillage.me>
2024-10-18 14:15:49 +0200

    get uma access token

--- dv_utils/solid_utils.py
+++ dv_utils/solid_utils.py
@@ -1,4 +1,6 @@
 import requests
+import json
+import base64
 from dv_utils import audit_log, LogLevel
 
 def get_token(webId: str, appId: str, user_name: str, password: str) -> str:
@@ -15,8 +17,8 @@ def get_token(webId: str, appId: str, user_name: str, password: str) -> str:
 Fetches permission ticket to access a resource through UMA flow
 returns (uma_uri, permission ticket)
 """
-def get_permission_ticket(pod_url: str, path: str) -> tuple[str, str]:
-  res = requests.get(f"{pod_url}{'' if pod_url.endswith('/') else '/'}{path}")
+def get_permission_ticket(resource_uri: str) -> tuple[str, str]:
+  res = requests.get(resource_uri)
 
   if res.status_code != 401:
     audit_log(f"Expected status code 401, got {res.status_code}")
@@ -60,3 +62,67 @@ def get_all_access_grants(vc_server: str, solid_id_token: str) -> list[dict]:
   res_json = res.json()
   print(f"got response from vc {res.status_code}: {res_json}")
   return res_json
+
+def get_uma_token(solid_idp_token: str, resource_uri: str, access_grant: dict):
+  # Call 1: get premission ticket from uma
+  (uma_uri, permission_ticket) = get_permission_ticket(resource_uri)
+
+  # Call 2: get uma configuration
+  uma_configuration = get_uma_configuration(uma_uri)
+  uma_token_endpoint = uma_configuration['token_endpoint']
+
+  # Call 3: get uma token without scopes
+  uma_unscoped_token = __request_uma_unscoped_token(solid_idp_token, uma_token_endpoint, permission_ticket) 
+
+  # Call 4: get uma token with scopes
+  return __request_uma_scoped_token(uma_token_endpoint, permission_ticket, access_grant, uma_unscoped_token['access_token'])
+
+
+def __request_uma_unscoped_token(solid_idp_token: str, uma_token_endpoint:str, permission_ticket: str) -> str:
+  payload = {
+    'ticket': permission_ticket,
+    'grant_type': 'urn:ietf:params:oauth:grant-type:uma-ticket',
+    'claim_token': solid_idp_token,
+    'claim_token_format': 'http://openid.net/specs/openid-connect-core-1_0.html#IDToken'
+  }
+
+  headers = {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+
+  res = requests.post(uma_token_endpoint, payload, headers=headers)
+  
+  if not res.ok:
+    audit_log(f"Could not get unscoped uma token. Got [{res.status_code}]: {res.text}", LogLevel.ERROR)
+    return ""
+  
+  return res.json()['access_token']
+
+def __request_uma_scoped_token(uma_token_endpoint: str, permission_ticket: str, access_grant: dict, unscoped_token: str) -> str:
+  verifiable_presentation = {
+    '@context': ['https://www.w3.org/2018/credentials/v1'],
+    'type': 'VerifiablePresentation',
+    'verifiableCredential': [access_grant]
+  }
+
+  vp_bytes = json.dumps(verifiable_presentation).encode()
+  vp_b64 = base64.b64encode(vp_bytes).decode()
+  
+  payload = {
+    'ticket': permission_ticket,
+    'grant_type': 'urn:ietf:params:oauth:grant-type:uma-ticket',
+    'claim_token': vp_b64,
+    'claim_token_format': f"https://www.w3.org/TR/vc-data-model/#json-ld&rpt={unscoped_token}"
+  }
+
+  headers = {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+
+  res = requests.post(uma_token_endpoint, payload, headers=headers)
+
+  if not res.ok:
+    audit_log(f"Could not get scoped uma token. Got [{res.status_code}]: {res.text}")
+    return ""
+  
+  res.json()['access_token']
