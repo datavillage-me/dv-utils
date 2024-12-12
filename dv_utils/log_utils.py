64fd7ede930253c78811ff2fe99ddb8db5eadaba
Michiel Rogissart <michiel@datavillage.me>
2024-12-12 10:47:17 +0100

    add LogMetadata class and global object

--- dv_utils/log_utils.py
+++ dv_utils/log_utils.py
@@ -9,20 +9,33 @@ from enum import Enum
 
 from .settings import settings as default_settings
 
-class bcolors:
-    DEFAULT = '\033[97m'
-    TRACE = '\033[97m'
-    DEBUG = '\033[97m'
-    INFO = '\033[94m'
-    WARN = '\033[93m'
-    ERROR = '\033[91m'
-
 class LogLevel(Enum):
     TRACE = 0
     DEBUG = 10
     INFO = 20
     WARN = 30
     ERROR = 40
+
+# Holds metadata about the current event that is added to every log statement
+# Do not create an object of this class in code
+class LogMetadata:
+    evt = None
+    evt_received = None
+    app_id = None
+    evt_stream = None
+    def __init__(self):
+        self.app_id = default_settings.config("DV_APP_ID", None)
+
+    def set_event(self, evt: dict, evt_stream: str, evt_received_ns: int | None = None):
+        self.evt = evt
+        self.evt_stream = evt_stream
+        self.evt_received = evt_received_ns if evt_received_ns is not None else time.time_ns()
+
+    def __iter__(self):
+        for key in self.__dict__:
+            yield key, getattr(self, key)
+
+_metadata = LogMetadata
 
 def get_loki_url() -> str:
     return default_settings.config("DV_LOKI", "http://loki.datavillage.svc.cluster.local:3100")
@@ -34,12 +47,16 @@ def get_app_namespace() -> str | None:
     else:
         return f'app-{cage_id}'
 
+def set_event(evt: dict, evt_received_ns: int | None = None):
+    _metadata.set_event(evt, evt_received_ns)
+
 def create_body(log: str, level: LogLevel, **kwargs):
     log_dict = dict()
     for k, v in kwargs.items():
         log_dict[k] = str(v)
 
     log_dict = {'msg': log}
+    log_dict.update(dict(_metadata))
     log_dict.update({'level': level.name})
     log_dict.update({'timestamp': time.time_ns()})
 
